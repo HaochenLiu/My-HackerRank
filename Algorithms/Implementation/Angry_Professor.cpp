6d83503e364ce2a4c8b2818b7d6b4a8dba82e9bd
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-08 19:29:59 -0800

    Update Angry_Professor.cpp

--- Algorithms/Implementation/Angry_Professor.cpp
+++ Algorithms/Implementation/Angry_Professor.cpp
@@ -1,1 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int T;
+    cin>>T;
+    while(T--) {
+        int N;
+        int K;
+        cin>>N>>K;
+        int cnt = 0;
+        for(int i = 0; i < N; i++) {
+            int t;
+            cin>>t;
+            if(t <= 0) {
+                cnt++;
+            }
+        }
+        if(cnt < K) {
+            cout<<"YES"<<endl;
+        } else {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}

e5bbe91f5571afdf9e1531adfd9855b89bac781f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-22 20:34:32 -0800

    Create Java_End-of-file.java

---
+++ Java/Introduction/Java_End-of-file.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 1;
+        while(sc.hasNext()) {
+            String s = sc.nextLine();
+            System.out.println(cnt + " " + s);
+            cnt++;
+        }
+    }
+}

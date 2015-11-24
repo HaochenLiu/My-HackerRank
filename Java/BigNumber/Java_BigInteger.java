054d07dd6248f422c400322a62e90173c13fcfe8
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-24 01:43:23 -0800

    Update Java_BigInteger.java

--- Java/BigNumber/Java_BigInteger.java
+++ Java/BigNumber/Java_BigInteger.java
@@ -1,1 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sa = sc.next();
+        String sb = sc.next();
+        BigInteger a = new BigInteger(sa);
+        BigInteger b = new BigInteger(sb);
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}

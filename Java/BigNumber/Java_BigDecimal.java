a80b20921c9361c0caec931e2e2df6ddd160fb7a
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-25 15:57:20 -0800

    Update Java_BigDecimal.java

--- Java/BigNumber/Java_BigDecimal.java
+++ Java/BigNumber/Java_BigDecimal.java
@@ -9,17 +9,17 @@ class Solution {
             return y.getKey().compareTo(x.getKey());
         }
     }
-
+    
     public static void main(String []argh) {
-		Scanner cin = new Scanner(System.in);
-		int T = cin.nextInt();
-
+        Scanner cin = new Scanner(System.in);
+        int T = cin.nextInt();
+        
         ArrayList<Pair<BigDecimal, String>> a = new ArrayList<Pair<BigDecimal, String>>();
-		for(int i = 0; i < T; i++){
-			String s = cin.next();
-			a.add(new Pair<BigDecimal, String>(new BigDecimal(s), s));
-		}
-
+        for(int i = 0; i < T; i++){
+            String s = cin.next();
+            a.add(new Pair<BigDecimal, String>(new BigDecimal(s), s));
+        }
+        
         Collections.sort(a, new EmpSortByEmpId());
         for(int i = 0; i < T; i++) {
             System.out.println(a.get(i).getValue());

5e4ffe6db6989b2930071e60ff3955bfc7b38cf2
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-27 23:14:41 -0800

    Update Security-Message_Space_and_Ciphertext_Space.cpp

--- Security/Terminology-and-Concepts/Security-Message_Space_and_Ciphertext_Space.cpp
+++ Security/Terminology-and-Concepts/Security-Message_Space_and_Ciphertext_Space.cpp
@@ -1,1 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    for(int i = 0; i < s.size(); i++) {
+        cout<<char(((s[i] - '0') + 1) % 10 + '0');
+    }
+    cout<<endl;
+
+    return 0;
+}

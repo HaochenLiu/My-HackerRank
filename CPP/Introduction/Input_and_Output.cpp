620fb6983907963b1e72814e0150487adbb1be27
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-27 16:18:37 -0800

    Create Input_and_Output.cpp

---
+++ CPP/Introduction/Input_and_Output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int A;
+    int B;
+    int C;
+    cin>>A>>B>>C;
+    cout<<A + B + C<<endl;
+    return 0;
+}

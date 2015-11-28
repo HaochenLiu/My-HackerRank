14b6defd7823619706591bf919b4e9d873a72353
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-28 01:19:08 -0800

    Create Rectangle_Area.cpp

---
+++ CPP/Inheritance/Rectangle_Area.cpp
@@ -0,0 +1,21 @@
+class Rectangle{
+protected:
+    int width;
+    int height;
+
+public:
+    void Input() {
+        scanf("%d%d", &width, &height);
+    }
+
+    void Display() {
+        printf("%d %d\n", width, height);
+    }
+};
+
+class RectangleArea: public Rectangle {
+public:
+    void Display() {
+        printf("%d\n",width * height);
+    }
+};
